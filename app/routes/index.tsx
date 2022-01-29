28c56df64aada193d3c683e0fb7e341d9e473418
Gustavo Santos <gustavofsantos@outlook.com>
2022-01-29 15:22:34 +0000

    fix temporal server

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,21 @@
-export default function Index() {
+import { ActionFunction, Form } from "remix";
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  console.log(formData);
+};
+
+export default function IndexPage() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <article>
+      <h1>My survey</h1>
+
+      <Form>
+        <label htmlFor="response">What you think of this?</label>
+        <input id="response" name="response" type="text" required />
+
+        <button type="submit">Submit</button>
+      </Form>
+    </article>
   );
 }
